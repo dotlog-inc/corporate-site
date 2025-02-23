542c5b3024f61023604dcccb72f395d25d679544
秋人 <syuuto20030912@icloud.com>
2025-02-23 17:18:37 +0900

    下書き作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,70 @@
 import Image from "next/image";
+import Link from "next/link" ;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now モリタはると アホンダラ
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return(
+    <>
+    <main>
+      <section id="philo" className="container">
+        <h1>PHILOSOPHY</h1>
+        <div className="box">
+          <h2>Dot Log</h2>
+            <p>企業名の『Dot Log』とは、あるプログラミング言語においてメッセージを出力する
+              ために使用される"console.log()"から由来しており、（）の中に入力されたものがスマー
+              トフォンやパソコンに出力（表示）されます
+              私たちは、わくわくする世界を創るためのアイデアを思いつく限り（　）の中に入
+              れ、世界にどんどん出力していくというミッションを掲げ、日々取り組んでおりま
+              す。</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Yamada
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →　大野 ジュンペイ
-        </a>
-      </footer>
-    </div>
+          <div> 
+            <h2>VISION</h2>
+              <p>迷ったら、わくわくするほうへ</p>
+          </div>
+
+          <div> 
+            <h2>MISSION</h2>
+             <p>今日よりちょっと優しい明日を</p>
+           </div> 
+
+          <div>
+            <h2>VALUE</h2>
+              {/* 四つの図入れる */}
+          </div>
+      </section>
+
+      <section id="service" className="container">
+        <div>
+          <h1>SERVICE</h1>
+        </div>
+          <div>
+            <h3>Alzuchi</h3>
+              <p>
+                心理カウンセラー監修のAIを活用したメンタルヘル<br>
+                </br>スケアアプリ「AIzuchi」AIとの会話を通じて自分を<br>
+                </br>もっと知る事ができ、頭の中を整理することができる<br>
+                </br>全く新しいジャーナリングアプリ             
+              </p>
+          </div>
+          <div>
+            <h3>企業ブランディング</h3>
+              <p>
+                Z世代ならでは視点から企業の「ブランディング」の<br>
+                </br>側面を整理し、提供するサービスや会社自体に、より<br>
+                </br>誇りを持っていただけるようサポートいたします。<br>
+                </br>サブスク型ホームページ制作サービス<br>
+                </br>「Grab」
+              </p>
+          </div>
+          <div>
+            <h3>SNSメディア</h3>
+              <p>
+                Instagram・TikTokにて、お悩み解消する情報やお出<br>
+                </br>かけ情報など、人生をより豊かにするための情報を発<br>
+                </br>信しております。
+              </p>
+          </div>
+      </section>
+    </main>
+    </>
   );
 }
