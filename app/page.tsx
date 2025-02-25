dddbc67300ba1f078aa90425f2a692de640c2c0c
秋人 <syuuto20030912@icloud.com>
2025-02-25 16:32:03 +0900

    下書き作成

--- app/page.tsx
+++ app/page.tsx
@@ -35,26 +35,24 @@ export default function Home() {
           <h1>PHILOSOPHY</h1>
           <div className="box">
            <h2>Dot Log</h2>
-            <p>企業名の『Dot Log』とは、あるプログラミング言語においてメッセージを出力する
-              ために使用される"console.log()"から由来しており、（）の中に入力されたものがスマー
-              トフォンやパソコンに出力（表示）されます
-              私たちは、わくわくする世界を創るためのアイデアを思いつく限り（　）の中に入
-              れ、世界にどんどん出力していくというミッションを掲げ、日々取り組んでおりま
-              す。</p>
+            <p>企業名の『Dot Log』とは、あるプログラミング言語においてメッセージを出力するために使用される"console.log()"から由来しており、（）の中に入力されたものがスマートフォンやパソコンに出力（表示）されます。<br></br>
+              私たちは、わくわくする世界を創るためのアイデアを思いつく限り（　）の中に入れ、世界にどんどん出力していくというミッションを掲げ、日々取り組んでおります。</p>
           </div>
-          <div> 
-            <h2>VISION</h2>
-              <p>迷ったら、わくわくするほうへ</p>
+          <div className="philos">
+          <div className="philo-1 title"> 
+            <h3>VISION</h3>
+              <p>迷ったら、わくわくするほうへ。</p>
           </div>
 
-          <div> 
-            <h2>MISSION</h2>
+          <div className="philo-2 title"> 
+            <h3>MISSION</h3>
              <p>今日よりちょっと優しい明日を</p>
            </div> 
 
-          <div>
-            <h2>VALUE</h2>
+          <div className="philo-3 title">
+            <h3>VALUE</h3>
               {/* 四つの図入れる */}
+          </div>
           </div>
       </section>
 
@@ -62,7 +60,7 @@ export default function Home() {
         <section id="service" className="container">
         <h1>SERVICE</h1>
           <div>
-            <h3>Alzuchi</h3>
+            <h2>Alzuchi</h2>
               <p>
                 心理カウンセラー監修のAIを活用したメンタルヘル<br>
                 </br>スケアアプリ「AIzuchi」AIとの会話を通じて自分を<br>
@@ -71,7 +69,7 @@ export default function Home() {
               </p>
           </div>
           <div>
-            <h3>企業ブランディング</h3>
+            <h2>企業ブランディング</h2>
               <p>
                 Z世代ならでは視点から企業の「ブランディング」の<br>
                 </br>側面を整理し、提供するサービスや会社自体に、より<br>
@@ -81,7 +79,7 @@ export default function Home() {
               </p>
           </div>
           <div>
-            <h3>SNSメディア</h3>
+            <h2>SNSメディア</h2>
               <p>
                 Instagram・TikTokにて、お悩み解消する情報やお出<br>
                 </br>かけ情報など、人生をより豊かにするための情報を発<br>
