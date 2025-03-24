ac16e2f2218e55ddaca3feffb77bd44b4c409bf4
junjun <dotjp.1345@gmail.com>
2025-03-24 11:35:58 +0900

    修正完了

--- app/component/contactCard.tsx
+++ app/component/contactCard.tsx
@@ -1,78 +1,125 @@
-"use client"
+"use client";
 
-import React, { useRef } from 'react'
-import { FormEvent } from "react";
-import { AnimatedDiv} from "./AnimatedDiv";
-
+import React, { useRef } from "react";
+import AnimatedDiv from "./AnimatedDiv";
 const ContactCard = () => {
-    const nameRef=useRef<HTMLInputElement>(null);
-    const emailRef=useRef<HTMLInputElement>(null);
-    const messageRef=useRef<HTMLTextAreaElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault();
-        console.log(nameRef.current?.value);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(nameRef.current?.value);
 
-        let data = {
-            name: nameRef.current?.value,
-            email: emailRef.current?.value,
-            message: messageRef.current?.value,
-        }
-
-        await fetch("api/contact",{
-            method:"POST",
-            headers:{
-                Accept:"application/json, text/plain",
-                "Content-Type": "application/json",    
-            },
-            body: JSON.stringify(data),
-        }).then((res)=>{
-            if(res.status === 200) console.log("メール送信成功")
-        }); 
+    const data = {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value,
+      message: messageRef.current?.value,
     };
 
-  return (
-    <div id="contact" className="bg-gradient-to-br from-sky-200 to-red-200 md:px-[48px] px-[24px] pb-[96px] h-[1500px]">
-        <div className="flex justify-center mb-[200px]">
-           <p className="about1">CONTACT</p>
-        </div>
-        <div className="md:px-[48px] px-[0px] color1">
-            <div className="font-face bg-white rounded-xl md:p-[96px] p-[48px] flex flex-col justify-center drop-shadow-2xl">
-                <AnimatedDiv><h4 className="text-center mb-[24px] text-base sm:text-lg md:text-[28px] leading-[1.5] tracking-widest font-bold">弊社へのお問い合わせは<br></br>こちらのフォームより承っております。</h4></AnimatedDiv>
-                <AnimatedDiv><p className="text-center mb-[48px] text-xs sm:text-sm md:text-base font-bold">ご返信に3営業日ほどお時間をいただいております。<br></br>3営業日を過ぎても返信がない場合は、お手数ですが再度お問い合わせをお願いいたします。</p></AnimatedDiv>
-                <form action="" className="flex flex-col justify-center" onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
-                    <div className="mb-6">
-                        <p className="font-black inline-block">お名前</p>
-                        <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
-                        <input type="text" placeholder="Your Name" className="bg-stone-100 h-[50px] w-full rounded-xl border border-stone-300 p-[12px]" required ref={nameRef}/>
-                    </div>
-                    <div className="mb-6">
-                        <p className="font-black inline-block">メールアドレス</p>
-                        <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
-                        <input type="email" placeholder="Email Addres" className="bg-stone-100 h-[50px] w-full rounded-xl border border-stone-300 p-[12px]" required ref={emailRef}/>
-                    </div>
-                    <div className="mb-6">
-                        <p className="font-black inline-block">電話番号</p>
-                        <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
-                        <input type="tel" placeholder="000-0000-0000" pattern="[\d\-]*" className="bg-stone-100 h-[50px] w-full rounded-xl border border-stone-300 p-[12px]" required/>
-                    </div>
-                    <div className="mb-12">
-                        <p className="font-black inline-block">お問い合わせ内容</p>
-                        <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
-                        <textarea name="" id="" placeholder="Put message here" className="bg-stone-100 h-[160px] w-full rounded-xl border border-stone-300 p-[10px]" required ref={messageRef}></textarea>
-                    </div>
-                    <label  htmlFor="checkbox" className="flex justify-center">
-                        <input id="checkbok" type="checkbox" required />
-                        <span className="text-xs sm:text-base">プライバシーポリシーに同意して送信する</span>
-                    </label>
-                    <div className="flex justify-center mt-[10px]">
-                        <button type="submit" className="text-white bg-black rounded-lg font-black h-[50px] drop-shadow-2xl duration-200 hover:drop-shadow-none hover:translate-x-1.5 hover:translate-y-1.5 w-[180px] sm:w-[200px]">この内容で送信する</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-  )
-}
+    await fetch("api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.status === 200) console.log("メール送信成功");
+    });
+  };
 
-export default ContactCard
+  return (
+    <div
+      id="contact"
+      className="bg-gradient-to-br from-sky-200 to-red-200 md:px-[48px] px-[24px] pb-[96px] h-[1500px]"
+    >
+      <div className="flex justify-center mb-[200px]">
+        <p className="about1">CONTACT</p>
+      </div>
+      <div className="md:px-[48px] px-[0px] color1">
+        <div className="font-face bg-white rounded-xl md:p-[96px] p-[48px] flex flex-col justify-center drop-shadow-2xl">
+          <AnimatedDiv>
+            <h4 className="text-center mb-[24px] text-base sm:text-lg md:text-[28px] leading-[1.5] tracking-widest font-bold">
+              弊社へのお問い合わせは<br></br>
+              こちらのフォームより承っております。
+            </h4>
+          </AnimatedDiv>
+          <AnimatedDiv>
+            <p className="text-center mb-[48px] text-xs sm:text-sm md:text-base font-bold">
+              ご返信に3営業日ほどお時間をいただいております。<br></br>
+              3営業日を過ぎても返信がない場合は、お手数ですが再度お問い合わせをお願いいたします。
+            </p>
+          </AnimatedDiv>
+          <form
+            action=""
+            className="flex flex-col justify-center"
+            onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
+          >
+            <div className="mb-6">
+              <p className="font-black inline-block">お名前</p>
+              <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="bg-stone-100 h-[50px] w-full rounded-xl border border-stone-300 p-[12px]"
+                required
+                ref={nameRef}
+              />
+            </div>
+            <div className="mb-6">
+              <p className="font-black inline-block">メールアドレス</p>
+              <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
+              <input
+                type="email"
+                placeholder="Email Addres"
+                className="bg-stone-100 h-[50px] w-full rounded-xl border border-stone-300 p-[12px]"
+                required
+                ref={emailRef}
+              />
+            </div>
+            <div className="mb-6">
+              <p className="font-black inline-block">電話番号</p>
+              <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
+              <input
+                type="tel"
+                placeholder="000-0000-0000"
+                pattern="[\d\-]*"
+                className="bg-stone-100 h-[50px] w-full rounded-xl border border-stone-300 p-[12px]"
+                required
+              />
+            </div>
+            <div className="mb-12">
+              <p className="font-black inline-block">お問い合わせ内容</p>
+              <p className="text-red-500 font-black inline-block ml-[5px]">*</p>
+              <textarea
+                name=""
+                id=""
+                placeholder="Put message here"
+                className="bg-stone-100 h-[160px] w-full rounded-xl border border-stone-300 p-[10px]"
+                required
+                ref={messageRef}
+              ></textarea>
+            </div>
+            <label htmlFor="checkbox" className="flex justify-center">
+              <input id="checkbok" type="checkbox" required />
+              <span className="text-xs sm:text-base">
+                プライバシーポリシーに同意して送信する
+              </span>
+            </label>
+            <div className="flex justify-center mt-[10px]">
+              <button
+                type="submit"
+                className="text-white bg-black rounded-lg font-black h-[50px] drop-shadow-2xl duration-200 hover:drop-shadow-none hover:translate-x-1.5 hover:translate-y-1.5 w-[180px] sm:w-[200px]"
+              >
+                この内容で送信する
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
--- app/component/headerCard.tsx
+++ app/component/headerCard.tsx
@@ -1,69 +1,124 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
-import Link from "next/link" ;
+import Link from "next/link";
 
 const HeaderCard = () => {
-        const [isWide, setIsWide] = useState<boolean>(false);
-    
-        useEffect(() => {
-        // 初回レンダリング時 & リサイズ時に横幅を判定
-        const checkWidth = () => {
-            setIsWide(window.innerWidth >= 768);
-        };
-    
-        checkWidth(); // 初回判定
-        window.addEventListener("resize", checkWidth);
-    
-        return () => window.removeEventListener("resize", checkWidth);
-        }, []);
+  const [isWide, setIsWide] = useState<boolean>(false);
 
-        const [isOpen, setIsOpen] = useState(false);
-    
+  useEffect(() => {
+    // 初回レンダリング時 & リサイズ時に横幅を判定
+    const checkWidth = () => {
+      setIsWide(window.innerWidth >= 768);
+    };
 
-    return (
+    checkWidth(); // 初回判定
+    window.addEventListener("resize", checkWidth);
+
+    return () => window.removeEventListener("resize", checkWidth);
+  }, []);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
     <header className="sticky top-0 flex mx-6 h-[88px]">
-            <h1 className="font-body text-sky-500 font-[1000] inline-block text-2xl my-4 flex items-center justify-center">Dot Log inc.</h1>
-            {isWide ? 
-            <div className="font-body inline-block font-bold text-sky-500 ml-auto flex items-center">
-                <Link href="" className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200">TOP</Link>
-                <Link href="#philo" className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200">PHILOSOPHY</Link>
-                <Link href="#service" className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200">SERVICE</Link>
-                <Link href="#news" className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200">NEWS</Link>
-                <Link href="#about" className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200">ABOUT</Link>
-                <Link href="#contact" className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200">CONTACT</Link>
-            </div>
-            :
-            <div className="ml-auto flex items-center">
-                <div className="">
-                    <button
-                        className="flex flex-col space-y-1 p-2"
-                        onClick={() => setIsOpen(!isOpen)}
-                    >
-                        <div className="w-8 h-1 bg-sky-500"></div>
-                        <div className="w-8 h-1 bg-sky-500"></div>
-                        <div className="w-8 h-1 bg-sky-500"></div>
-                    </button>
-                </div>
-            
-                {/* メニュー表示 */}
-                {isOpen && (
-                    <nav className="absolute top-16 left-0 w-full bg-white shadow-lg p-4">
-                    <ul className="space-y-2">
-                        <li><a href="/" className="block">HOME</a></li>
-                        <li><a href="#philo" className="block">PHILOSOPHY</a></li>
-                        <li><a href="#service" className="block">SERVICE</a></li>
-                        <li><a href="#about" className="block">ABOUT</a></li>
-                        <li><a href="#news" className="block">NEWS</a></li>
-                        <li><a href="#contact" className="block">CONTACT</a></li>
-                    </ul>
-                    </nav>)}
-            </div>
+      <h1 className="font-body text-sky-500 font-[1000] inline-block text-2xl my-4 items-center justify-center">
+        Dot Log inc.
+      </h1>
+      {isWide ? (
+        <div className="font-body inline-block font-bold text-sky-500 ml-auto items-center">
+          <Link
+            href=""
+            className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200"
+          >
+            TOP
+          </Link>
+          <Link
+            href="#philo"
+            className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200"
+          >
+            PHILOSOPHY
+          </Link>
+          <Link
+            href="#service"
+            className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200"
+          >
+            SERVICE
+          </Link>
+          <Link
+            href="#news"
+            className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200"
+          >
+            NEWS
+          </Link>
+          <Link
+            href="#about"
+            className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200"
+          >
+            ABOUT
+          </Link>
+          <Link
+            href="#contact"
+            className="text-base lg:text-lg inline-block px-3 py-4 relative hover:scale-90 duration-200"
+          >
+            CONTACT
+          </Link>
+        </div>
+      ) : (
+        <div className="ml-auto flex items-center">
+          <div className="">
+            <button
+              className="flex flex-col space-y-1 p-2"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <div className="w-8 h-1 bg-sky-500"></div>
+              <div className="w-8 h-1 bg-sky-500"></div>
+              <div className="w-8 h-1 bg-sky-500"></div>
+            </button>
+          </div>
 
-            }
-        </header>
-    )
-}
+          {/* メニュー表示 */}
+          {isOpen && (
+            <nav className="absolute top-16 left-0 w-full bg-white shadow-lg p-4">
+              <ul className="space-y-2">
+                <li>
+                  <Link href="/" className="block">
+                    HOME
+                  </Link>
+                </li>
+                <li>
+                  <a href="#philo" className="block">
+                    PHILOSOPHY
+                  </a>
+                </li>
+                <li>
+                  <a href="#service" className="block">
+                    SERVICE
+                  </a>
+                </li>
+                <li>
+                  <a href="#about" className="block">
+                    ABOUT
+                  </a>
+                </li>
+                <li>
+                  <a href="#news" className="block">
+                    NEWS
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact" className="block">
+                    CONTACT
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
+      )}
+    </header>
+  );
+};
 
-export default HeaderCard
+export default HeaderCard;
